5d8a977eb0c4f9d3481a5408fc6d97c832945a4d
lxdong <328740754@163.com>
2019-05-27 00:22:31 +0800

    提交程序修改

---
+++ server/ColtSmart.Data/Util.cs
@@ -0,0 +1,11 @@
+﻿namespace ColtSmart.Data
+{
+    public static class Util
+    {
+        
+        public static int GetId(this ISqlExecutor sqlExecutor, string tableName)
+        {
+            return sqlExecutor.ExecuteScalar<int>("select max(Id) from " + tableName) + 1;
+        }
+    }
+}
