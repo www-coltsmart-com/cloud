e2c41323689f155658950fe2dd6b817e530cc0da
longshaoye <jim-lung@163.com>
2019-05-29 01:30:44 +0800

    补传设备服务接口和实现类

---
+++ server/ColtSmart.Service.Impl/Impl/DeviceService.cs
@@ -0,0 +1,42 @@
+﻿using ColtSmart.Data;
+using ColtSmart.Entity;
+using ColtSmart.Service.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ColtSmart.Service.Impl.Impl
+{
+    public class DeviceService : IDeviceService
+    {
+        private readonly ISqlExecutor sqlExecutor = null;
+
+        public DeviceService(ISqlExecutor sqlExecutor)
+        {
+            this.sqlExecutor = sqlExecutor;
+        }
+
+        public PagedResult<Device> GetDevices(int page, int pageSize, string deviceName)
+        {
+            StringBuilder sqlBuilder = new StringBuilder("SELECT * FROM device");
+            object param = null;
+            if (!string.IsNullOrEmpty(deviceName))
+            {
+                sqlBuilder.Append(" WHERE \"DeviceName\" like @DeviceName");
+                param = new { DeviceName = string.Format("{0}%", deviceName.Trim()) };
+            }
+            return sqlExecutor.QueryPage<Device>(sqlBuilder.ToString(), page, pageSize, param).ToPagedResult();
+        }
+
+        public int DeleteDevice(int id)
+        {
+            Device device = sqlExecutor.Find<Device>(new { Id = id }).FirstOrDefault();
+            if (device != null)
+            {
+                return sqlExecutor.Delete<Device>(device);
+            }
+            return 0;
+        }
+    }
+}
---
+++ server/ColtSmart.Service/Service/IDeviceService.cs
@@ -0,0 +1,14 @@
+﻿using ColtSmart.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ColtSmart.Service.Service
+{
+    public interface IDeviceService
+    {
+        PagedResult<Device> GetDevices(int page, int pageSize, string deviceName);
+
+        int DeleteDevice(int id);
+    }
+}
