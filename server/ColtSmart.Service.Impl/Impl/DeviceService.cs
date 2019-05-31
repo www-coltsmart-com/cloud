cf561807c45411b337b450c273934d9c36f14328
longshaoye <jim-lung@163.com>
2019-05-31 23:36:07 +0800

    修改设备列表功能，异步调用

--- server/ColtSmart.Service.Impl/Impl/DeviceService.cs
+++ server/ColtSmart.Service.Impl/Impl/DeviceService.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ColtSmart.Service.Impl
 {
-    public class DeviceService: IDeviceService
+    public class DeviceService : IDeviceService
     {
         private readonly ISqlExecutor sqlExecutor;
 
@@ -18,26 +18,49 @@ namespace ColtSmart.Service.Impl
         public async Task<Device> GetDevice(string deviceId)
         {
             var device = await this.sqlExecutor.FindAsync<Device>(new { DeviceId = deviceId });
-
             return device.FirstOrDefault();
         }
 
         public async Task<Device> GetDevice(string deviceId, string userNo)
         {
             var device = await this.sqlExecutor.FindAsync<Device>(new { DeviceId = deviceId, UserOwn = userNo });
-
             return device.FirstOrDefault();
         }
-
-        public async Task<PagedResult<Device>> GetDevices(int page, int pageSize, string deviceName)
+        /// <summary>
+        /// 获取当前用户的设备列表，管理员可以查看所有设备，注册用户只能看到自己名下的设备
+        /// </summary>
+        /// <param name="page">页数</param>
+        /// <param name="pageSize">页面大小</param>
+        /// <param name="userNo">使用人</param>
+        /// <param name="deviceId">设备编号</param>
+        /// <param name="deviceName">设备名称</param>
+        /// <returns>设备列表</returns>
+        public async Task<PagedResult<Device>> GetDevices(int page, int pageSize, string userNo, string deviceId, string deviceName)
         {
-            StringBuilder sqlBuilder = new StringBuilder("SELECT * FROM device");
-            object param = null;
+            StringBuilder sqlBuilder = new StringBuilder();
+            if (!string.IsNullOrEmpty(userNo))
+            {
+                if (sqlBuilder.Length > 0) sqlBuilder.Append(" AND ");
+                sqlBuilder.Append("\"UserOwn\" = @UserOwn");
+            }
+            if (!string.IsNullOrEmpty(deviceId))
+            {
+                if (sqlBuilder.Length > 0) sqlBuilder.Append(" AND ");
+                sqlBuilder.Append("\"DeviceId\" LIKE @DeviceId");
+            }
             if (!string.IsNullOrEmpty(deviceName))
             {
-                sqlBuilder.Append(" WHERE \"DeviceName\" like @DeviceName");
-                param = new { DeviceName = string.Format("{0}%", deviceName.Trim()) };
+                if (sqlBuilder.Length > 0) sqlBuilder.Append(" AND ");
+                sqlBuilder.Append("\"DeviceName\" LIKE @DeviceName");
             }
+            object param = new
+            {
+                UserOwn = string.IsNullOrEmpty(userNo) ? "" : userNo.Trim(),
+                DeviceId = string.IsNullOrEmpty(deviceId) ? "%" : string.Format("{0}%", deviceId.Trim()),
+                DeviceName = string.IsNullOrEmpty(deviceName) ? "%" : string.Format("{0}%", deviceName.Trim())
+            };
+            if (sqlBuilder.Length > 0) sqlBuilder.Insert(0, " WHERE ");
+            sqlBuilder.Insert(0, "SELECT * FROM device");
             var results = await sqlExecutor.QueryPageAsync<Device>(sqlBuilder.ToString(), page, pageSize, param);
             return results.ToPagedResult();
         }
@@ -53,27 +76,33 @@ namespace ColtSmart.Service.Impl
             {
                 #region
                 DeviceId = device.DeviceId,
-                DeviceType=device.DeviceType,
-                IsGetway=device.IsGetway,
-                DeviceName=device.DeviceName,
-                IsOnline=device.IsOnline,
-                InDate=device.InDate,
-                UserOwn=device.UserOwn,
-                Gps=device.Gps,
-                Version=device.Version,
-                ComPortNum=device.ComPortNum
+                DeviceType = device.DeviceType,
+                IsGetway = device.IsGetway,
+                DeviceName = device.DeviceName,
+                IsOnline = device.IsOnline,
+                InDate = device.InDate,
+                UserOwn = device.UserOwn,
+                Gps = device.Gps,
+                Version = device.Version,
+                ComPortNum = device.ComPortNum
                 #endregion
             }, commandType: System.Data.CommandType.Text);
         }
 
-        public async Task Delete(int id)
+        /// <summary>
+        /// 删除设备
+        /// </summary>
+        /// <param name="id">设备唯一标识</param>
+        /// <returns>影响行数</returns>
+        public async Task<int> Delete(int id)
         {
             var results = await this.sqlExecutor.FindAsync<Device>(new { Id = id });
             var device = results.FirstOrDefault();
             if (device != null)
             {
-                await this.sqlExecutor.DeleteAsync(device);
+               return await this.sqlExecutor.DeleteAsync(device);
             }
+            return 0;
         }
     }
 }
--- server/Coltsmart.Portal/Controllers/DeviceController.cs
+++ server/Coltsmart.Portal/Controllers/DeviceController.cs
@@ -16,19 +16,33 @@ namespace Coltsmart.Portal.Controllers
         {
             this.deviceService = deviceService;
         }
-
+        /// <summary>
+        /// 获取设备列表
+        /// </summary>
+        /// <param name="page"></param>
+        /// <param name="size"></param>
+        /// <param name="deviceId">设备编号</param>
+        /// <param name="deviceName">设备名称</param>
+        /// <returns></returns>
         [HttpGet]
         [Route("api/devices")]
-        public async Task<PagedResult<Device>> Get(int page, int size, string devicename)
+        public async Task<PagedResult<Device>> Get(int page, int size, string deviceId, string deviceName)
         {
-            return await deviceService.GetDevices(page, size, devicename);
+            //TODO:添加权限控制
+            string userNo = "";
+
+            //判断当前用户是管理员或普通用户
+            //如是管理员，则将userNo置空，表示所有设备列表均可见
+            //否则，将userNo保留原值，表示只对名下设备列表可见
+
+            return await deviceService.GetDevices(page, size, userNo, deviceId, deviceName);
         }
 
         [HttpDelete]
         [Route("api/devices/{id}")]
-        public void Delete(int id)
+        public async Task<int> Delete(int id)
         {
-            deviceService.Delete(id);
+           return await deviceService.Delete(id);
         }
     }
 }
