82a4244af148594408c17263c7fb02af543d8b5a
lxdong <328740754@163.com>
2019-06-19 18:54:52 +0800

    提交index.vue

---
+++ ui/src/views/layout/Index.vue
@@ -0,0 +1,18 @@
+
+<template>
+    <section>
+        首页内容显示
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'HomeIndex',
+    data: function () {
+    },
+    methods: {
+    },
+    mounted: function () {
+    }
+}
+</script>
