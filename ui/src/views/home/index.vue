2fd0bc3ee3308708c8ce463c6eb0bca74da5e148
lxdong <328740754@163.com>
2019-06-26 23:52:59 +0800

    提交程序调整

---
+++ ui/src/views/home/index.vue
@@ -0,0 +1,19 @@
+
+<template>
+    <section>
+        首页内容显示
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'HomeIndex',
+    data: function () {
+        return [];
+    },
+    methods: {
+    },
+    mounted: function () {
+    }
+}
+</script>
