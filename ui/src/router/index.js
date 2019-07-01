7c19d773cdb08b5cf7bda73612b22a4d991545de
longshaoye <jim-lung@163.com>
2019-07-02 01:22:55 +0800

    resolve url # problem

--- ui/src/router/index.js
+++ ui/src/router/index.js
@@ -10,32 +10,33 @@ import AccountInfo from '@/views/settings/AccountInfo'
 Vue.use(Router)
 
 const router = new Router({
+  mode: 'history',
   routes: [
-    { 
-      path: '/login', 
+    {
+      path: '/login',
       name: 'login',
-      component: Login, 
-      hidden: true 
+      component: Login,
+      hidden: true
     },
-    { 
-      path: '/register', 
+    {
+      path: '/register',
       name: 'register',
-      component: Register, 
-      hidden: true 
+      component: Register,
+      hidden: true
     },
     {
       path: '/',
       name: '首页',
-      component: Home, 
+      component: Home,
       hidden: false,
-      children:[        
+      children: [
         {
           path: '/account',
           name: '账户信息',
           hidden: true,
           component: AccountInfo
         },
-        
+
         {
           path: '/password',
           name: '修改密码',
@@ -66,23 +67,22 @@ const router = new Router({
 })
 
 const tokenIsExpired = function () {
-  if(localStorage.expiration)
-  {
-      var expirationDt = new Date(localStorage.expiration)
-      if (expirationDt.getTime() < (new Date()).getTime()) {
-          return true
-      }
+  if (localStorage.expiration) {
+    var expirationDt = new Date(localStorage.expiration)
+    if (expirationDt.getTime() < (new Date()).getTime()) {
+      return true
+    }
   }
   return false
 }
 
 const checkRegistion = function () {
-  return axios.get('api/Login/checkri',{
+  return axios.get('api/Login/checkri', {
     baseURL: process.env.BASE_URL,
     timeout: 600000
-}).then((response) => {
+  }).then((response) => {
     return Promise.resolve(response.data)
-  }).catch(error=> Promise.reject(error))
+  }).catch(error => Promise.reject(error))
 }
 
 router.beforeEach((to, from, next) => {
@@ -90,35 +90,12 @@ router.beforeEach((to, from, next) => {
   if (localStorage.token) {
     next()
   } else {
-    if (to.name === 'login') 
-    {
+    if (to.name === 'login') {
       next()
-      /*
-      checkRegistion().then(result => {        
-        if (result == true) {
-          next()
-        }
-        else {
-          next({name: 'register'})
-        }
-      }).catch(error => {
-        this.$message({
-          message: error.message,
-          type: 'error'
-        })
-      })
-      */
-    } 
-    else if (to.name === 'register') {
+    } else if (to.name === 'register') {
       next()
-    }
-    /*
-    else if(to.name="resetpassword"){
-      next();
-    }
-    */
-    else {
-      next({name: 'login'});
+    } else {
+      next({ name: 'login' })
     }
   }
 })
